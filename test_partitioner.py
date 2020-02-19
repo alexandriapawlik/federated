d190b2f88fc277319a81c0c055ac47c248a9dbf5
alexandriapawlik <alexp17000@gmail.com>
2020-02-19 18:43:06 -0500

    update to IID partition

--- test_partitioner.py
+++ test_partitioner.py
@@ -1,18 +1,18 @@
 import by_label
-# import iid
-
-# print()
-# print("IID partitioning:")
-# print("60,000 samples randomized distributed to 100 clients")
-# print()
-
-# partitioner = iid.IID_Partitioner()
-# partitioner.go()
+import iid
 
 print()
-print("Non-IID partitioning:")
-print("60,000 samples divided by label, split into 200 shards, and randomly distributed to 100 clients")
+print("IID partitioning:")
+print("60,000 samples randomly distributed to 100 clients")
 print()
 
-partitioner = by_label.Label_Partitioner()
+partitioner = iid.IID_Partitioner()
 partitioner.go()
+
+# print()
+# print("Non-IID partitioning:")
+# print("60,000 samples divided by label, split into 200 shards, and randomly distributed to 100 clients")
+# print()
+
+# partitioner = by_label.Label_Partitioner()
+# partitioner.go()
