5491053257e2f1cddd1bfd25ecb21b9ea773da70
alexandriapawlik <alexp17000@gmail.com>
2020-03-16 21:07:55 -0400

    update to Slurm script

--- test_tff.sh
+++ test_tff.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-#SBATCH --job-name fl_distill
+#SBATCH --job-name tff_main
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=3g
 #SBATCH --time=40:00:00
-#SBATCH --account=stats_dept1
+#SBATCH --account=tewaria1
 #SBATCH --partition=standard
 #SBATCH --mail-user=apawlik@umich.edu
 #SBATCH --mail-type=BEGIN,END
-#SBATCH --output=gl_results/lr_tune.%A_%a.log
+#SBATCH --output=results/lr_tune.%A_%a.log
 
-#SBATCH --array=0-23
+#SBATCH --array=0-1
 
-mkdir gl_results/${SLURM_ARRAY_JOB_ID}
+mkdir results/${SLURM_ARRAY_JOB_ID}
 
 python tff_main.py $SLURM_ARRAY_TASK_ID
 
-cp gl_results/lr_tune.${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.log gl_results/${SLURM_ARRAY_JOB_ID}
+cp results/lr_tune.${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.log results/${SLURM_ARRAY_JOB_ID}
