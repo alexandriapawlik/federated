cc19de0ab84ce0589df2d24efbaeddc9ffd49395
alexandriapawlik <alexp17000@gmail.com>
2020-03-18 15:46:21 -0400

    request more memory in slurm script

--- test_tff.sh
+++ test_tff.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=tff_partition
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=3g
+#SBATCH --mem-per-cpu=5g
 #SBATCH --time=40:00:00
 #SBATCH --account=tewaria1
 #SBATCH --partition=standard
