10eaf1a282ff814b0a163e2403ff029f7402e1b8
alexandriapawlik <apawlik@umich.edu>
2020-03-25 13:15:21 -0400

    first batch without errors

--- test_tff.sh
+++ test_tff.sh
@@ -14,7 +14,8 @@
 #SBATCH --array=1-2
 
 module load python3.7-anaconda
-module load cuda/10.0.130 cudnn/10.0-v7.6
+module load cudnn/10.0-v7.6
+module load cuda/10.1.105
 module list
 
 mkdir results/${SLURM_ARRAY_JOB_ID}
