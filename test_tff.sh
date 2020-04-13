29b62fcf19914c097cda5f3ebba1aca318672c8e
alexandriapawlik <apawlik@umich.edu>
2020-04-12 22:59:04 -0400

    batch 5317327-invalid-label-s1

--- test_tff.sh
+++ test_tff.sh
@@ -3,13 +3,13 @@
 #SBATCH --job-name=tff_partition
 #SBATCH --nodes=4
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=8g
-#SBATCH --time=50:00:00
+#SBATCH --mem-per-cpu=6g
+#SBATCH --time=48:00:00
 #SBATCH --account=tewaria1
 #SBATCH --partition=standard
 #SBATCH --mail-user=apawlik@umich.edu
 #SBATCH --mail-type=END
-#SBATCH --output=results/%A/s.%a.log
+#SBATCH --output=results/s.%a.log
 
 #SBATCH --array=1
 
@@ -21,3 +21,5 @@ module list
 mkdir results/${SLURM_ARRAY_JOB_ID}
 
 python tff_main.py $SLURM_ARRAY_TASK_ID > results/${SLURM_ARRAY_JOB_ID}/tff.${SLURM_ARRAY_JOB_ID}.${SLURM_ARRAY_TASK_ID}.out
+mv results/s.$SLURM_ARRAY_TASK_ID.log results/${SLURM_ARRAY_JOB_ID}/
+
