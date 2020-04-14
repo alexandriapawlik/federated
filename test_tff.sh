b770ef693a23415c4e96a7d24f65b4281ac31769
alexandriapawlik <apawlik@umich.edu>
2020-04-14 15:46:01 -0400

    batch 5324828-debug-label

--- test_tff.sh
+++ test_tff.sh
@@ -11,7 +11,7 @@
 #SBATCH --mail-type=END
 #SBATCH --output=results/tff.%A.%a.out
 
-#SBATCH --array=1-14
+#SBATCH --array=1-5
 
 module load python3.7-anaconda
 module load cudnn/10.0-v7.6
@@ -20,5 +20,5 @@ module list
 
 mkdir results/${SLURM_ARRAY_JOB_ID}
 
-python tff_main.py $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_JOB_ID
+python tff_main.py $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_JOB_ID > debug.${SLURM_ARRAY_JOB_ID}.${SLURM_ARRAY_TASK_ID}.out
 mv results/tff.${SLURM_ARRAY_JOB_ID}.${SLURM_ARRAY_TASK_ID}.out results/${SLURM_ARRAY_JOB_ID}/
